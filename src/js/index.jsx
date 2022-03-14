5ea9eea4ce6a6e41371dc629986bad1a9347d4f6
Holley Voegtle <hvoegtle@gmail.com>
2022-03-14 15:46:21 -0700

    added containers

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
 import App from './app';
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const store = createStore(reducers, composeEnhancers(
+	applyMiddleware(
+		promiseMiddleware()
+	)
+));
 
+/* Update render method invoke */
 render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={ store }>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  //mode: 'development',
+
   context: path.join(__dirname, '/src'),
 
   entry: {
